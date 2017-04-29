7ae44ac0318d08753b01228fe2293fc84307a720
sihai <sihai@sihaideMacBook-Pro.local>
2017-04-29 12:23:21 +0800

    selector complete, add some tool method

--- myjQuery.js
+++ myjQuery.js
@@ -1,4 +1,4 @@
-(function(w){
+(function(w, d){
   var myjQuery = function(math){
     // 可以直接调用，无需手动进行new
     return new myjQuery.prototype.init(math);
@@ -15,58 +15,75 @@
     init: function(math){
       var math2trim = '',
           elements = [];
-      // if 空
+      
       if (!math) {
+        // 匹配：空
         return this;
-      }
-      // if $$对象
-      if (math[0] instanceof HTMLElement) {
-        return math;
-      }
-      // if DOM元素
-      if (math instanceof HTMLElement) {
-        this[0] = math;
-        this.length = 1;
-        this.selector = math.tagName.toLowerCase();
-        return this;
-      }
-      // if 不为空切不为DOM元素
-      math2trim = math.trim();
-      // if 匹配id
-      if (math2trim.charAt(0) === '#') {
-        this[0] = document.getElementById(math2trim.substring(1));
-        this.length = 1;
-      } else {
-      // if 不匹配id以外(div、.class)
-        elements = document.querySelectorAll(math2trim);
+      }else if(typeof math !== 'string'){
+        // 匹配：DOM || 循环嵌套封装对象 || 函数
+        if (math['selector']) {
+          // 匹配：循环嵌套$$情况，例如：$$($('.a'))
+          return math;
+        }else if(math instanceof HTMLElement){
+          // 匹配：单个DOM
+          this[0] = math;
+          this.length = 1;
+          this.selector = math.tagName.toLowerCase();
 
-        for (var i = 0; i < elements.length; i++) {
-          this[i] = elements[i];
+          return this;
+        }else if(typeof math === 'object'){
+          // 匹配：一组DOM
+          for (var i = 0; i < math.length; i++) {
+            this[i] = math[i];
+          }
+          this.length = math.length;
+          this.selector = math[0].tagName.toLowerCase();
+
+          return this;
+        }else if(myjQuery.isFunction(math)){
+          // console.log(myjQuery.isFunction(math))
+          // 匹配：函数
+          this.ready(math)
         }
-        
-        this.length = elements.length;
-      }
+      }else if(typeof math === 'string'){
 
-      this.selector = math;
-      return this;
+        // 匹配：字符串
+        math2trim = myjQuery.trim(math);
+        // 匹配：id
+        if (math2trim.charAt(0) === '#') {
+          this[0] = d.getElementById(math2trim.substring(1));
+          this.length = 1;
+        } else {
+        // 匹配：标签 || class
+          elements = d.querySelectorAll(math2trim);
+
+          for (var i = 0; i < elements.length; i++) {
+            this[i] = elements[i];
+          }
+          
+          this.length = elements.length;
+        }
+        this.selector = math;
+        return this;
+      }
     },
-    // 去前后空格
-    trim(str){
-      return str.replace(/(^\s*)|(\s*)$/g,'');
-    },
-    // 把DOM节点变为数组
-    toArray: function(){
-      return Array.prototype.slice.call(this)
+    ready: function(fn){
+      if (fn && typeof fn === 'function') {
+        d.addEventListener('DOMContentLoaded', function(){
+          fn();
+        })
+      }
     },
     // 判断是否有className
     hasClass : function(cls) {
       if (!cls) return;
       // 去空格
-      var cls = this.trim(cls);
+      var cls = myjQuery.trim(cls);
       // 节点className排除前后空格
       var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-      for(var v of this.toArray(this)){
-        if (!reg.test(v.className)) {
+      for (var i = 0; i < this.length; i++) {
+        console.log(reg.test(this[i].className))
+        if (!reg.test(this[i].className)) {
           return false;
           break;
         }
@@ -77,7 +94,7 @@
     addClass: function(cls){
       if (!cls) return;
       // 去空格
-      var cls = this.trim(cls);
+      var cls = myjQuery.trim(cls);
 
       for (var i = 0; i < this.length; i++) {
         this[i].classList.add(cls);
@@ -89,7 +106,7 @@
     removeClass: function(cls){
       if (!cls) return;
       // 去空格
-      var cls = this.trim(cls);
+      var cls = myjQuery.trim(cls);
 
       for (var i = 0; i < this.length; i++) {
         this[i].classList.remove(cls);
@@ -150,9 +167,21 @@
       return this.pushStack(this[0].parentElement);
     }
   }
-
+  // 工具方法
+  // 是否为函数
+  myjQuery.isFunction = function(str){
+    return typeof str === 'function';
+  }
+  // 清除前后空格
+  myjQuery.trim = function(str){
+    return str.replace(/(^\s*)|(\s*)$/g,'');
+  }
+  // 转数组
+  myjQuery.toArray = function(arr){
+    return Array.prototype.slice.call(arr);
+  },
   // new myjQuery.prototype.init()后返回的是init构造的对象，它的原型上没有myjQuery.prototype的方法，需手动指定
   myjQuery.prototype.init.prototype = myjQuery.prototype;
   // 把构造函数挂载到window上
   w.myjQuery = w.$$ = myjQuery;
-})(window)
+})(window, document)
